74dfd126e6533c5676bf195a0e630a7eeedd370b
fdesi <francesco.desimone@canonical.com>
2025-09-08 01:48:11 +0200

    add keybind to snacks

--- lua/rfc/pickers/_snacks.lua
+++ lua/rfc/pickers/_snacks.lua
@@ -1,8 +1,21 @@
 local M = {}
+local function open_rfc(picker, open_mode)
+  local fetcher = require("rfc.fetcher")
+  local item = picker:current()
+  if not item then
+    return
+  end
 
+  picker:close()
+  local num = tonumber(item.value:match("^(%d+)"))
+  if num then
+    fetcher.open_rfc_document(num, open_mode)
+  end
+end
+
+---@param rfc_data string[]
 function M.show(rfc_data)
   local snacks = require("snacks")
-  local fetcher = require("rfc.fetcher")
 
   snacks.picker.pick({
     prompt = "RFC Index> ",
@@ -12,13 +25,34 @@ function M.show(rfc_data)
     format = "text",
     layout = { preset = "select" },
     preview = "none",
-    confirm = function(picker, item)
-      picker:close()
-      local num = tonumber(item.value:match("^(%d+)"))
-      if num then
-        fetcher.open_rfc_document(num)
-      end
-    end,
+
+    actions = {
+      open = function(picker)
+        open_rfc(picker, nil)
+      end,
+      open_vsplit = function(picker)
+        open_rfc(picker, "vnew")
+      end,
+      open_split = function(picker)
+        open_rfc(picker, "new")
+      end,
+    },
+
+    confirm = "open",
+    win = {
+      input = {
+        keys = {
+          ["<C-v>"] = { "open_vsplit", mode = { "i", "n" } },
+          ["<C-x>"] = { "open_split", mode = { "i", "n" } },
+        },
+      },
+      list = {
+        keys = {
+          ["<C-v>"] = "open_vsplit",
+          ["<C-x>"] = "open_split",
+        },
+      },
+    },
   })
 end
 
